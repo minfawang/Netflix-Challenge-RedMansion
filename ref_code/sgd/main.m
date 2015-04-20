49def4eb27c84f08337d4d446d5418b2c33bba6e
neicullyn <neicullyn@gmail.com>
2015-04-20 09:34:29 -0700

    MATLAB

--- ref_code/sgd/main.m
+++ ref_code/sgd/main.m
@@ -1,19 +1,20 @@
-data = load('data.txt');
+clear;
+load;
 
 % Parameters
-n_users = 943;
-n_movies = 1682;
+n_users = max(data(:,1));
+n_movies = max(data(:,2));
 K = 20;
-n_iterations = 100;
+n_iterations = 20;
 
 M = size(data, 1);
 
-lambda = 0.01 / (n_user * n_movies);
+lambda = 0.01 ;
 learning_rate = 0.005;
 % since the output of the function is gradient / lambda
 % our rate is learning_rate * lambda
-rate = lambda * learning_rate;
-mu = mean(data(:,3));
+rate = learning_rate;
+mu = mean(data(:,4));
 
 % Randomly Initialize all the Xs
 U = random('unif', 0, 1, [K, n_users]);
@@ -21,24 +22,38 @@ V = random('unif', 0, 1, [K, n_movies]);
 A = random('unif', 0, 1, [1, n_users]);
 B = random('unif', 0, 1, [1, n_movies]);
 
-Y = data(:,3);
+Y = data(:,4);
 I = data(:,1);
 J = data(:,2);
 
+shrink = 1 - rate * lambda
+
 for i_loop = 1:n_iterations
+    fprintf('start iter %d', i_loop);
     perm = randperm(M);
+    tic;
     for i_perm = 1:M
         index = perm(i_perm);
         i = I(index, 1);
         j = J(index, 1);
         y = Y(index, 1);
-        [gU, gV, gA, gB] = sgd_gradient(y, U, V, A, B, lambda, mu, i, j);
+        [gUi, gVj, gAi, gBj] = sgd_gradient(y, U, V, A, B, lambda, mu, i, j);
         
-        U = U - rate * gU;
-        V = V - rate * gV;
-        A = A - rate * gA;
-        B = B - rate * gB;
+        U(:,i) = U(:,i) - rate * gUi;
+        V(:,j) = V(:,j) - rate * gVj;
+        A(:,i) = A(:,i) - rate * gAi;
+        B(:,j) = B(:,j) - rate * gBj;
+        
+        if mod(i_perm, 10000) == 0
+            toc;tic;
+            i_perm
+        end 
     end
+    U = shrink * U;
+    V = shrink * V;
+    A = shrink * A;
+    B = shrink * B;
+    
     
     Y_guess = zeros(M, 1);
     for i_row = 1:M
@@ -52,5 +67,3 @@ for i_loop = 1:n_iterations
     err2
 end
 
-% Save to 'matlab.mat'
-save;
--- ref_code/sgd/sgd_gradient.m
+++ ref_code/sgd/sgd_gradient.m
@@ -1,13 +1,10 @@
-function [ gU, gV, gA, gB ] = sgd_gradient( y, U, V, A, B, lambda, mu, i, j)
+function [ gUi, gVj, gAi, gBj ] = sgd_gradient( y, U, V, A, B, lambda, mu, i, j)
     % for efficience, output gradient / lambda
-    gU = U;
-    gV = V;
-    gA = A;
-    gB = B;
-    pFpX = 2 * (y - mu - (U(:,i)' * V(:,j) + A(:,i) + B(:,j))) / lambda;
-    gU(:,i) = gU(:,i) - pFpX * V(:,j);
-    gV(:,j) = gV(:,j) - pFpX * U(:,i);
-    gA(:,i) = gA(:,i) - pFpX;
-    gB(:,j) = gB(:,j) - pFpX;
+    pFpX = 2 * (y - mu - (U(:,i)' * V(:,j) + A(:,i) + B(:,j)));
+
+    gUi = - pFpX * V(:,j);
+    gVj = - pFpX * U(:,i);
+    gAi =  - pFpX;
+    gBj =  - pFpX;
 end
 
