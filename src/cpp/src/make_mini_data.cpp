3760d2ada3e5268055b75562e596ac370bf57d25
minfawan <minfa.wang@gmail.com>
2015-05-04 12:09:47 -0700

    fix some bugs and restructure rbm

--- src/cpp/src/make_mini_data.cpp
+++ src/cpp/src/make_mini_data.cpp
@@ -8,52 +8,12 @@
 #ifndef __MAKE_MINI_DATA
 #define __MAKE_MINI_DATA
 
-#define DIV 3
+#define DIV 10
 #define PROB_IN_MINI 20
 
 
 using namespace std;
 
-
-// void split_mini (string prefix) {
-// 	srand(1);
-// 	string mini_name = "/Users/voiceup/Git/RedMansion-Netflix/src/data/" + prefix + "_data.data";
-// 	string main_name = "/Users/voiceup/Git/RedMansion-Netflix/src/data/" + prefix + "_main.data";
-// 	string prob_name = "/Users/voiceup/Git/RedMansion-Netflix/src/data/" + prefix + "_prob.data";
-// 	ofstream mini_main, mini_prob;
-
-// 	record_array recMini;
-// 	recMini.load(mini_name.c_str());
-
-// 	int maxU = 458293 / DIV;
-// 	int maxM = 17770 / DIV;
-// 	int prob_size = recMini.size / PROB_IN_MINI;
-// 	int prob_idx[prob_size];
-// 	int main_prob_idx[recMini.size];
-
-// 	for (int i = 0; i < prob_size; i++) {
-// 		prob_idx[i] = i;
-// 	}
-// 	for (int i = 0; i < recMini.size; i++) {
-// 		main_prob_idx[i] = 0;
-// 	}
-
-// 	for (int i = prob_size; i < recMini.size; i++) {
-// 		int rand_num = rand() % i;
-// 		if (rand_num < prob_size) {
-// 			prob_idx[rand() % prob_size] = i;
-// 		}
-// 	}
-
-// 	for (int i = 0; i < prob_size; i++) {
-// 		main_prob_idx[prob_idx[i]] = 1;
-// 	}
-
-
-// 	mini_main.close();
-// 	mini_prob.close();
-
-// }
 
 
 
@@ -69,8 +29,7 @@ void split_mini (string prefix) {
 	record_array recMini;
 	recMini.load(mini_name.c_str());
 
-	// int maxU = 458293 / DIV;
-	// int maxM = 17770 / DIV;
+
 	int prob_size = recMini.size / PROB_IN_MINI;
 	int prob_idx[prob_size];
 	short* main_prob_idx = new short[recMini.size];
@@ -130,18 +89,6 @@ void make_mini(string prefix) {
 	// size of main data is 98291670
 	// cout << "The size: " << recMain.size << endl;
 
-	// int maxU = 0;
-	// int maxM = 0;
-	// for (int i = 0; i < recMain.size; i++) {
-	// 	record r = recMain.data[i];
-	// 	if (r.user > maxU)
-	// 		maxU = r.user;
-	// 	if (r.movie > maxM)
-	// 		maxM = r.movie;
-	// }
-
-	// cout << "Max user: " << maxU << endl;
-	// cout << "Max movie: " << maxM << endl;
 
 	int maxU = 458293 / DIV;
 	int maxM = 17770 / DIV;
@@ -159,8 +106,8 @@ void make_mini(string prefix) {
 
 
 int main () {
-	string prefix = "mid";
-	// make_mini(prefix);
+	string prefix = "mini";
+	make_mini(prefix);
 	split_mini(prefix);
 
 	return 0;
